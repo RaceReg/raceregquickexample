3f0097a8e4a8bb0141267ac855c9f609a9b2ec4c
jacksonporter <jackson_porter@outlook.com>
2018-10-07 13:39:22 -0600

    Added basic MVVM

--- raceregquickexample/RaceRegQuickExample/Model/Model.cs
+++ raceregquickexample/RaceRegQuickExample/Model/Model.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using RaceReqQuickExample.ViewModel;
 
-namespace Tests
+namespace RaceRegQuickExample.Model
 {
-    public class Class1
+    class Model
     {
     }
 }
